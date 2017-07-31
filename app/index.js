40a39ea020600605d0e8c286a260bad756df4616
navjot singh <mail.navjot77@gmail.com>
2017-07-31 11:42:55 -0500

    Hooked redux store extension with react app

--- app/index.js
+++ app/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom'
 import getRoutes from './config/route'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import users from 'redux/modules/user'
 import {checkIfAuthed} from './helpers/auth'
 
 
-const store=createStore(users,applyMiddleware(thunk));
+const store=createStore(users,compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension():(f)=>f
+));
 
 
 function checkAuth(){
